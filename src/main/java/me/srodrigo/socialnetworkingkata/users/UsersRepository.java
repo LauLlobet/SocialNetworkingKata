725ec9b148e18a5e46a8984be539760dfd00f665
srodrigo <sergio.rodrigo.royo@gmail.com>
2016-03-09 21:47:43 +0100

    Made the test pass again. Changed language level to 1.8

--- src/main/java/me/srodrigo/socialnetworkingkata/users/UsersRepository.java
+++ src/main/java/me/srodrigo/socialnetworkingkata/users/UsersRepository.java
@@ -1,14 +1,18 @@
 package me.srodrigo.socialnetworkingkata.users;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class UsersRepository {
+
+	private List<User> users = new ArrayList<>();
 
 	public User findOrCreate(String username) {
 		throw new UnsupportedOperationException();
 	}
 
 	public List<User> findAll() {
-		throw new UnsupportedOperationException();
+		return Collections.unmodifiableList(users);
 	}
 }
