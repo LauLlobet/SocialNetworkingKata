d8f9f7237b2e961d2b2209196d6755cc840c7a5e
srodrigo <sergio.rodrigo.royo@gmail.com>
2016-03-11 19:52:34 +0100

    Made the test pass by creating a list of posts and storing the new post

--- src/main/java/me/srodrigo/socialnetworkingkata/posts/Post.java
+++ src/main/java/me/srodrigo/socialnetworkingkata/posts/Post.java
@@ -2,7 +2,28 @@ package me.srodrigo.socialnetworkingkata.posts;
 
 public class Post {
 
+	private final String username;
+	private final String message;
+
 	public Post(String username, String message) {
-		throw new UnsupportedOperationException();
+		this.username = username;
+		this.message = message;
+	}
+
+	@Override public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Post post = (Post) o;
+
+		if (username != null ? !username.equals(post.username) : post.username != null) return false;
+		return message != null ? message.equals(post.message) : post.message == null;
+
+	}
+
+	@Override public int hashCode() {
+		int result = username != null ? username.hashCode() : 0;
+		result = 31 * result + (message != null ? message.hashCode() : 0);
+		return result;
 	}
 }
--- src/main/java/me/srodrigo/socialnetworkingkata/posts/PostsRepository.java
+++ src/main/java/me/srodrigo/socialnetworkingkata/posts/PostsRepository.java
@@ -2,15 +2,21 @@ package me.srodrigo.socialnetworkingkata.posts;
 
 import me.srodrigo.socialnetworkingkata.users.User;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class PostsRepository {
 
+	private List<Post> posts = new ArrayList<>();
+
 	public Post createPostForUser(String message, User user) {
-		throw new UnsupportedOperationException();
+		Post newPost = new Post(user.username(), message);
+		posts.add(newPost);
+		return newPost;
 	}
 
 	public List<Post> findAll() {
-		throw new UnsupportedOperationException();
+		return Collections.unmodifiableList(posts);
 	}
 }
