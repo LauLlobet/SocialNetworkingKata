d8d7aa26fbff5378b55b6da7995f3c8ff1507aa1
srodrigo <sergio.rodrigo.royo@gmail.com>
2016-03-12 20:24:51 +0100

    Made the test pass with a dummy implementation for the moment

--- src/main/java/me/srodrigo/socialnetworkingkata/posts/PastDateFormatter.java
+++ src/main/java/me/srodrigo/socialnetworkingkata/posts/PastDateFormatter.java
@@ -2,7 +2,9 @@ package me.srodrigo.socialnetworkingkata.posts;
 
 public class PastDateFormatter {
 
+	private static final String ONE_MINUTE_AGO = "1 minute ago";
+
 	public String format(long pastDate) {
-		throw new UnsupportedOperationException();
+		return ONE_MINUTE_AGO;
 	}
 }
