46e5ffe4b081287ed2d82ecd4352e5358e799bd6
srodrigo <sergio.rodrigo.royo@gmail.com>
2016-03-13 09:48:31 +0100

    Made the test pass by handling seconds formatting

--- src/main/java/me/srodrigo/socialnetworkingkata/posts/PastDateFormatter.java
+++ src/main/java/me/srodrigo/socialnetworkingkata/posts/PastDateFormatter.java
@@ -2,6 +2,8 @@ package me.srodrigo.socialnetworkingkata.posts;
 
 public class PastDateFormatter {
 
+	private static final String ONE_SECOND_AGO = "1 second ago";
+	private static final String N_SECONDS_AGO_FORMAT = "%d seconds ago";
 	private static final String ONE_MINUTE_AGO = "1 minute ago";
 	private static final String N_MINUTES_AGO_FORMAT = "%d minutes ago";
 
@@ -15,11 +17,27 @@ public class PastDateFormatter {
 		long now = clock.now();
 		long millisElapsed = now - pastDate;
 
-		if (millisElapsed <= Clock.MILLIS_IN_MINUTE) {
-			return ONE_MINUTE_AGO;
+		boolean lessThanAMinute = millisElapsed < Clock.MILLIS_IN_MINUTE;
+		if (lessThanAMinute) {
+			int seconds = (int) (millisElapsed / Clock.MILLIS_IN_SECOND);
+			return formatSeconds(seconds);
 		}
 
 		int minutes = (int) (millisElapsed / Clock.MILLIS_IN_MINUTE);
+		return formatMinutes(minutes);
+	}
+
+	private String formatSeconds(int seconds) {
+		if (seconds == 1) {
+			return ONE_SECOND_AGO;
+		}
+		return String.format(N_SECONDS_AGO_FORMAT, seconds);
+	}
+
+	private String formatMinutes(int minutes) {
+		if (minutes == 1) {
+			return ONE_MINUTE_AGO;
+		}
 		return String.format(N_MINUTES_AGO_FORMAT, minutes);
 	}
 }
