bc59f69def2bedfb074ccb2dea25492693d33844
srodrigo <sergio.rodrigo.royo@gmail.com>
2016-03-13 19:05:49 +0100

    Made the test pass. All green

--- src/main/java/me/srodrigo/socialnetworkingkata/posts/WallPostFormatter.java
+++ src/main/java/me/srodrigo/socialnetworkingkata/posts/WallPostFormatter.java
@@ -2,11 +2,17 @@ package me.srodrigo.socialnetworkingkata.posts;
 
 public class WallPostFormatter implements PostFormatter {
 
+	private static final String POST_FORMAT = "%s - %s (%s)";
+
+	private final PastDateFormatter pastDateFormatter;
+
 	public WallPostFormatter(PastDateFormatter pastDateFormatter) {
+		this.pastDateFormatter = pastDateFormatter;
 	}
 
 	@Override
 	public String format(Post post) {
-		throw new UnsupportedOperationException();
+		String dateFormatted = pastDateFormatter.format(post.date());
+		return String.format(POST_FORMAT, post.username(), post.message(), dateFormatted);
 	}
 }
