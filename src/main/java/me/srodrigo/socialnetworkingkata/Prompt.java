8302c9c79b3ae9c3505cac5116ef7b26382db2f9
srodrigo <sergio.rodrigo.royo@gmail.com>
2016-03-13 12:58:52 +0100

    Made the test pass by parsing the 'follows' command and executing the service method

--- src/main/java/me/srodrigo/socialnetworkingkata/Prompt.java
+++ src/main/java/me/srodrigo/socialnetworkingkata/Prompt.java
@@ -5,6 +5,7 @@ import me.srodrigo.socialnetworkingkata.users.UserService;
 public class Prompt {
 
 	private static final String POST_MESSAGE_KEYWORD = "->";
+	private static final String FOLLOW_KEYWORD = "follows";
 	private final UserService userService;
 
 	public Prompt(UserService userService) {
@@ -14,6 +15,8 @@ public class Prompt {
 	public void readCommand(String command) {
 		if (isPostingCommand(command)) {
 			createPost(command);
+		} else if (isFollowingCommand(command)) {
+			followUser(command);
 		} else {
 			showTimeline(command);
 		}
@@ -21,6 +24,10 @@ public class Prompt {
 
 	private boolean isPostingCommand(String command) {
 		return command.contains(POST_MESSAGE_KEYWORD);
+	}
+
+	private boolean isFollowingCommand(String command) {
+		return command.contains(FOLLOW_KEYWORD);
 	}
 
 	private void createPost(String command) {
@@ -34,6 +41,17 @@ public class Prompt {
 		return new CreatePostParameters(tokens[0], tokens[1]);
 	}
 
+	private void followUser(String command) {
+		FollowUserParameters parameters = extractFollowUserParameters(command);
+		userService.follow(parameters.follower, parameters.followed);
+	}
+
+	private FollowUserParameters extractFollowUserParameters(String command) {
+		String keywordWithSpaces = String.format(" %s ", FOLLOW_KEYWORD);
+		String[] tokens = command.split(keywordWithSpaces);
+		return new FollowUserParameters(tokens[0], tokens[1]);
+	}
+
 	private void showTimeline(String username) {
 		userService.showTimeline(username);
 	}
@@ -45,6 +63,16 @@ public class Prompt {
 		public CreatePostParameters(String username, String postMessage) {
 			this.username = username;
 			this.postMessage = postMessage;
+		}
+	}
+
+	private class FollowUserParameters {
+		String follower;
+		String followed;
+
+		public FollowUserParameters(String follower, String followed) {
+			this.follower = follower;
+			this.followed = followed;
 		}
 	}
 }
